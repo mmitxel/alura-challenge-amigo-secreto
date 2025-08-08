51c1428602b2db4ccf6f0ad151bca8328881ebbb
Mauricio Michel <mauriciomichel@gmail.com>
2025-08-08 01:17:56 -0600

    add functionality to save inputs in list and clear input field

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    console.log(amigos)
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        amigos.push(amigo)
+        document.getElementById("amigo").value = ""
+    }
+
+
+
+
+}
